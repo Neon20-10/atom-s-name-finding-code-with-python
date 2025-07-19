b236fcf40cd61d61fe0784bd802556b19bf0612f
Ohian Chowdhury Neon <advmr12@gmail.com>
2025-07-19 16:44:36 +0600

    Add files via upload

---
+++ atom's name finding code.py
@@ -0,0 +1,87 @@
+print("---------------------Welcome to my atom's name finding code--------------------")
+atom_num=int(input("Enter the Atomic number that's name you want to know (It must be in 1-40): "))
+if(atom_num==1):
+    print("The Atom's name is Hydrogen(H)")
+elif(atom_num==2):
+    print("The Atom's name is Helium(He)")
+elif(atom_num==3):
+    print("The Atom's name is Lithium(Li)")
+elif(atom_num==4):
+    print("The Atom's name is Beryllium(Be)")
+elif(atom_num==5):
+    print("The Atom's name is Boron(B)")
+elif(atom_num==6):
+    print("The Atom's name is Carbon(C)")
+elif(atom_num==7):
+    print("The Atom's name is Nitrogen(N)")
+elif(atom_num==8):
+    print("The Atom's name is Oxygen(O)")
+elif(atom_num==9):
+    print("The Atom's name is Fluorine(F)")
+elif(atom_num==10):
+    print("The Atom's name is Neon(Ne)")
+elif(atom_num==11):
+    print("The Atom's name is Sodium(Na)")
+elif(atom_num==12):
+    print("The Atom's name is Magnesium(Mg)")
+elif(atom_num==13):
+    print("The Atom's name is Aluminium(Al)")
+elif(atom_num==14):
+    print("The Atom's name is Silicon(Si)")
+elif(atom_num==15):
+    print("The Atom's name is Phosphorus(P)")
+elif(atom_num==16):
+    print("The Atom's name is Sulfur(S)")
+elif(atom_num==17):
+    print("The Atom's name is Cholorine(Cl)")
+elif(atom_num==18):
+    print("The Atom's name is Argon(Ar)")
+elif(atom_num==19):
+    print("The Atom's name is Potassium(K)")
+elif(atom_num==20):
+    print("The Atom's name is Calcium(Ca)")
+elif(atom_num==21):
+    print("The Atom's name is Scandium(Sc)")
+elif(atom_num==22):
+    print("The Atom's name is Titanium(Ti)")
+elif(atom_num==23):
+    print("The Atom's name is Vanadium(V)")
+elif(atom_num==24):
+    print("The Atom's name is Chromium(Cr)")
+elif(atom_num==25):
+    print("The Atom's name is Manganese(Mn)")
+elif(atom_num==26):
+    print("The Atom's name is Iron(Fe)")
+elif(atom_num==27):
+    print("The Atom's name is Cobalt(Co)")
+elif(atom_num==28):
+    print("The Atom's name is Nickel(Ni)")
+elif(atom_num==29):
+    print("The Atom's name is Copper(Cu)")
+elif(atom_num==30):
+    print("The Atom's name is Zinc(Zn)")
+elif(atom_num==31):
+    print("The Atom's name is Gallium(Ga)")
+elif(atom_num==32):
+    print("The Atom's name is Germenium(Ge)")
+elif(atom_num==33):
+    print("The Atom's name is Arsenic(As)")
+elif(atom_num==34):
+    print("The Atom's name is Selenium(Se)")
+elif(atom_num==35):
+    print("The Atom's name is Bromine(Br)")
+elif(atom_num==36):
+    print("The Atom's name is Krypton(Kr)")
+elif(atom_num==37):
+    print("The Atom's name is Rubidium(Rb)")
+elif(atom_num==38):
+    print("The Atom's name is Strontium(Sr)")
+elif(atom_num==39):
+    print("The Atom's name is Yttrium(Y)")
+elif(atom_num==40):
+    print("The Atom's name is Zirconium(Zr)")
+else:
+    print('***Your input seems invalid -_- .Please enter the number between 1-40***')
+print('-------------------------------------------------------------------------------')
+print('Thanks for using this code.\nLet me know if you like it ^_^')
+    
